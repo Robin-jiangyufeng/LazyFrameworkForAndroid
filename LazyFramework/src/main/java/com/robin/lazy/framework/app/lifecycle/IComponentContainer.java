6eedcdc480a924e9604d895323e89a56b89e1f8e
jiangyufeng <429257411@qq.com>
2017-07-04 17:07:49 +0800

    first add

---
+++ LazyFramework/src/main/java/com/robin/lazy/framework/app/lifecycle/IComponentContainer.java
@@ -0,0 +1,5 @@
+package com.robin.lazy.framework.app.lifecycle;
+
+public interface IComponentContainer {
+    public void addComponent(com.robin.lazy.framework.app.lifecycle.LifeCycleComponent component);
+}
